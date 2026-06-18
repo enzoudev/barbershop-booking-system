02525b8670a1ed7fc798381a9f1a3c38ff55d502
enzoudev <enzoroot6@gmail.com>
2026-06-18 16:30:56 -0300

    feat: add feature for register

---
+++ app/api/login/register/routes.ts
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+import { pool } from '@/lib/database'
+import  bcrypt from 'bcryptjs'
+
+
+export async function POST(req: Request) {
+    try {
+        const body = await req.json();
+        const {nome, email, password} = body;
+
+        if(!nome || !email || !password) {
+            return NextResponse.json( {error: 'Algum campo obrigatório está em branco'}, {status:400});
+        }
+
+        const [user]: any = await pool.query('SELECT id FROM usuarios WHERE email = ?', [email]);
+
+        if(user.length > 0) {
+            return NextResponse.json( {error: "Já existe uma conta com esse email"}, {status:400})
+        }
+
+
+        
+        const salt = await bcrypt.genSalt(10);
+        const passwordCripto = await bcrypt.hash(password, salt)
+
+        await pool.query('INSERT INTO usuarios (nome, email, senha) VALUES (?,?,?)', [nome, email, passwordCripto])
+
+        return NextResponse.json( {message: "Conta criada com sucesso!"}, {status:201})
+    } catch(error) {
+        console.error('Erro na API:', error)
+        return NextResponse.json({ error: 'Erro interno no servidor ao criar conta.' }, { status: 500 });
+    }
+}
