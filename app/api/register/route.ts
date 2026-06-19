0677c544f85a02d36ad0478c8f6fb1b31fdf7c5d
enzoudev <enzoroot6@gmail.com>
2026-06-19 19:39:28 -0300

    fix: connection client structure

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -4,15 +4,18 @@ import  bcrypt from 'bcryptjs'
 
 
 export async function POST(req: Request) {
-    const client = await pool.connect();
+
     try {
         const body = await req.json();
         const {name, email, password} = body;
 
         if(!name || !email || !password) {
             return NextResponse.json( {error: 'Algum campo obrigatório está em branco'}, {status:400});
-        }
+        }  
 
+        const client = await pool.connect();
+
+        try {
         const result = await client.query('SELECT id FROM users WHERE email = $1', [email]);
         const user = result.rows;
 
@@ -28,10 +31,14 @@ export async function POST(req: Request) {
         await client.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3)', [name, email, passwordCripto]);
 
         return NextResponse.json( {message: "Conta criada com sucesso!"}, {status:201})
+        } finally {
+            client.release();
+        }
+
+
+        
     } catch(error) {
         console.error('Erro na API:', error)
         return NextResponse.json({ error: 'Erro interno no servidor ao criar conta.' }, { status: 500 });
-    } finally {
-        client.release();
-    }
+    } 
 }
