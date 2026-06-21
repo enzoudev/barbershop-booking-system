13799600babaf7182c5e48ac3b40d81240a81505
enzoudev <enzoroot6@gmail.com>
2026-06-21 02:13:34 -0300

    fix: import auth

--- app/api/schedule/route.ts
+++ app/api/schedule/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { pool } from '@/lib/database'
-
+import { getUserIdFromToken } from '@/lib/auth'
 
 export async function POST(req: NextRequest) {
     
