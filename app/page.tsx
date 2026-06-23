12b8cbc460d474dc5ceecfc752808b94b14f8639
enzoudev <enzoroot6@gmail.com>
2026-06-23 16:58:39 -0300

    feat: add import to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import {Hero} from '@/components/home-page/hero'
 import { Plans } from '@/components/home-page/plans'
+import { HomePage } from '@/components/home-page/homePage'
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Hero/>
-      <Plans/>
+      <HomePage/>
+      {/* <Hero/>
+      <Plans/> */}
     </div>
   );
 }
