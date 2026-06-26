7df71f2c413e8a66bc3cdd6b57f68d31285487b2
enzoudev <enzoroot6@gmail.com>
2026-06-26 18:03:13 -0300

    feat: add link

--- components/common/NavBar.tsx
+++ components/common/NavBar.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from 'next/link';
 
 import Image from "next/image";
 import { useState } from 'react'
@@ -32,11 +33,14 @@ const customColor = "text-[oklch(70.7%_0.022_261.325)]";
       <div className="flex gap-6 items-center">
         <h1 className={`cursor-pointer text-sm ${hoverNav === "contate" ? "text-black" : customColor}`} onMouseEnter={() => setHoverNav("contate")} onMouseLeave={() => setHoverNav(null)}>Contate-nos</h1>
 
-        <h1 className={`text-sm text-[#243741]  flex justify-center items-center w-20 h-10 transition-colors duration-200 rounded-md cursor-pointer border border-gray-300 ${bgLogin ? "bg-[#e5e5d5]" : "bg-white"}`} 
-        onMouseEnter={() =>{ console.log("Entrou aqui:", bgLogin); setbgLogin(true)}}
-        onMouseLeave={() => setbgLogin(false)} 
-        >Login
-        </h1>
+        <Link 
+            href="/login"
+            className={`text-sm text-[#243741] flex justify-center items-center w-20 h-10 transition-colors duration-200 rounded-md cursor-pointer border border-gray-300 ${bgLogin ? "bg-[#e5e5d5]" : "bg-white"}`}
+            onMouseEnter={() => setbgLogin(true)}
+            onMouseLeave={() => setbgLogin(false)}
+        >
+            Login
+        </Link>
 
       </div>
     </nav>
