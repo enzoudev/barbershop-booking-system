72342db456c3d2a881f11904f3ad91a4839bacfe
enzoudev <enzoroot6@gmail.com>
2026-06-26 18:02:35 -0300

    chore: organization

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,16 @@ import Image from "next/image";
 import {Hero} from '@/components/home-page/hero'
 import { Plans } from '@/components/home-page/plans'
 
-import { LoginForm} from '@/components/login-page/loginForm'
+
 import { NavBar } from "@/components/common/NavBar"; 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      {/* <HomePage/> */}
+  
       
       <NavBar/>
       <Hero className={"w-full bg-[#0A0A0A] py-32 flex justify-center items-center border-b-1"}/>
       <Plans/>
-      {/* <LoginForm/> */}
     </div>
   );
 }
