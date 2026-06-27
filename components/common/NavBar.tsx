fe4c9b7963c870f6c3dda714c08c9adc341fcc8b
enzoudev <enzoroot6@gmail.com>
2026-06-27 17:27:05 -0300

    fix: delete console.log

--- components/common/NavBar.tsx
+++ components/common/NavBar.tsx
@@ -33,7 +33,7 @@ const customColor = "text-[oklch(70.7%_0.022_261.325)]";
         <h1 className={`cursor-pointer text-sm ${hoverNav === "contate" ? "text-black" : customColor}`} onMouseEnter={() => setHoverNav("contate")} onMouseLeave={() => setHoverNav(null)}>Contate-nos</h1>
 
         <h1 className={`text-sm text-[#243741]  flex justify-center items-center w-20 h-10 transition-colors duration-200 rounded-md cursor-pointer border border-gray-300 ${bgLogin ? "bg-[#e5e5d5]" : "bg-white"}`} 
-        onMouseEnter={() =>{ console.log("Entrou aqui:", bgLogin); setbgLogin(true)}}
+        onMouseEnter={() => setbgLogin(true)}
         onMouseLeave={() => setbgLogin(false)} 
         >Login
         </h1>
