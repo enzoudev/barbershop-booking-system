243a344aefc777997fe75105cf455b8abbc44fae
enzoudev <enzoroot6@gmail.com>
2026-06-28 18:27:28 -0300

    feat: finally handle

--- services/authService.ts
+++ services/authService.ts
@@ -1,15 +1,23 @@
+export async function authenticateUser(email: String, password: String) {
+    try {
+        const res = await fetch('/api/login', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({email, password}),
+        });
 
+        // Adicione isso para debugar o erro da sua API
+        if (!res.ok) {
+            const errorData = await res.json();
+            console.error("Erro retornado pela API:", errorData);
+            throw new Error(errorData.message || "Falha no login");
+        }
 
-export async function authenticateUser(credentials: any) {
-    const res = await fetch('/api/login', {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(credentials),
-    });
-
-    if(!res.ok) {
-        throw new Error("Falha no login")
+        const data = await res.json();
+        console.log("Sucesso, dados recebidos:", data);
+        return data;
+    } catch (err) {
+        console.error("Erro na comunicação:", err);
+        throw err;
     }
-
-    return res.json(); // Retorna o token, por exemplo
 }
