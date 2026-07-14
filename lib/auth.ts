1540f48289829af9416c7e0c1731b7b7d017fdc5
enzoudev <enzoroot6@gmail.com>
2026-07-13 23:39:18 -0300

    chore: org file

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,12 +2,11 @@ import { jwtVerify } from "jose";
 
 export async function getUserFromToken(token: string) {
     try {
-        // Precisamos codificar o segredo da mesma forma que fizemos na criação
         const secret = new TextEncoder().encode(process.env.JWT_SECRET);
         
         const { payload } = await jwtVerify(token, secret);
         
-        // Garanta que o TypeScript saiba que id e name existem no payload
+
         return { 
             id: payload.id as string, 
             name: payload.name as string 
