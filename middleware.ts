c3c5b165cb6ec041b76cbc922174d979cb4451c8
enzoudev <enzoroot6@gmail.com>
2026-07-17 17:57:57 -0300

    feat: add export config

--- middleware.ts
+++ middleware.ts
@@ -3,14 +3,17 @@ import type { NextRequest } from "next/server";
 import { jwtVerify } from "jose";
 
 export async function middleware(req: NextRequest)  {
+    console.log("Middleware rodando para:", req.nextUrl.pathname);
     const token = req.cookies.get('token')?.value;
+    console.log("Token detectado no middleware:", token);
     const path = req.nextUrl.pathname;
 
 
 
     const protectedRoutes = ['/perfil', '/dashboard','/schedules', '/scheduling'];
 
-    const isProtectedRoute = protectedRoutes.some((route) => req.nextUrl.pathname.startsWith(route));
+    const isProtectedRoute = protectedRoutes.some((route) => path.startsWith(route));
+
     if(isProtectedRoute) {
         if(!token) {
             return NextResponse.redirect(new URL('/login', req.url))
@@ -27,3 +30,12 @@ export async function middleware(req: NextRequest)  {
     }
     return NextResponse.next();
 }
+
+export const config = {
+  matcher: [
+    '/perfil', '/perfil/:path*',
+    '/dashboard', '/dashboard/:path*',
+    '/schedules', '/schedules/:path*',
+    '/scheduling', '/scheduling/:path*'
+  ],
+};
