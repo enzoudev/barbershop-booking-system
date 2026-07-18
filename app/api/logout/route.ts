134fb3f5f75b7578b79ec489efbb83630d07104f
enzoudev <enzoroot6@gmail.com>
2026-07-18 16:30:52 -0300

    feat: add api to logout

---
+++ app/api/logout/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from 'next/server';
+
+export async function POST() {
+  const response = NextResponse.json({ message: "Logout realizado" });
+  
+  response.cookies.set('token', '', { 
+    expires: new Date(0),
+    path: '/',
+    httpOnly: true 
+  });
+  
+  return response;
+}
