be3616bdeec8a200f6d933ef8d1b37d7f409c633
enzoudev <enzoroot6@gmail.com>
2026-07-20 23:06:32 -0300

    styles: add cursor-pointer in buttons

--- components/common/UserDropdown.tsx
+++ components/common/UserDropdown.tsx
@@ -22,7 +22,7 @@ export default function UserDropdown({ isOpen, onClose }: UserDropdownProps) {
   if (!isOpen) return null;
 
   return (
-    <div className="absolute left-0 mt-2 w-32 bg-white border border-gray-100 rounded-lg shadow-xl z-50 overflow-hidden text-sm">
+    <div className="absolute left-0 pt-2 w-32 bg-white border border-gray-100 rounded-lg shadow-xl z-50 overflow-hidden text-sm cursor-pointer">
       <Link 
         href="/dashboard" 
         className="block px-4 py-2 text-gray-700 hover:bg-gray-50"
@@ -35,7 +35,7 @@ export default function UserDropdown({ isOpen, onClose }: UserDropdownProps) {
           onLogout()
           onClose();
         }}
-        className="block w-full text-left px-4 py-2 text-red-600 hover:bg-red-50"
+        className="block w-full text-left px-4 py-2 text-red-600 hover:bg-red-50 cursor-pointer"
       >
         Sair
       </button>
