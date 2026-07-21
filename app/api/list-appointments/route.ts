3d648b37cc62939abbcba582c3bdc49fa900a103
enzoudev <enzoroot6@gmail.com>
2026-07-21 17:44:39 -0300

    feat: add api to get appointments user

---
+++ app/api/list-appointments/route.ts
@@ -0,0 +1,29 @@
+
+
+import { NextResponse, NextRequest } from "next/server";
+import { pool } from '@/lib/database'
+import { getUserFromToken } from "@/lib/auth";
+
+
+export default async function GET(req: NextRequest){
+    try {
+        const token = req.cookies.get('token')?.value
+        const user = await getUserFromToken(String(token));
+
+        if(!user) {
+            return NextResponse.json( {error: 'Usuário não logado'}, {status: 401});
+        }
+
+        const { rows } = await pool.query('SELECT created_at, date_time FROM APPOINTMENTS WHERE id_user = $1', [user.id]);
+
+        if(rows.length === 0) {
+            return NextResponse.json( {message: 'Nenhum agendamento feito!'}, {status: 400});
+        }
+
+
+        return NextResponse.json(rows, {status: 200})
+    } catch (err) {
+        console.error('Erro na API: ', err);
+        return NextResponse.json({ error: 'Erro interno do servidor' }, { status: 500 });
+    }
+}
