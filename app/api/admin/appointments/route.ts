b9741766fcab4c17d4f501a21c61cac44c5a964b
enzoudev <enzoroot6@gmail.com>
2026-07-22 13:22:36 -0300

    feat: add filter

--- app/api/admin/appointments/route.ts
+++ app/api/admin/appointments/route.ts
@@ -20,8 +20,29 @@ export async function GET(req: NextRequest) {
             return NextResponse.json( {message: "Usuário não possui cargo de admin"}, {status:403});
         }
 
+        const searchParams = req.nextUrl.searchParams;
+        const statusAppoint = searchParams.get('status');
+        const orderConsult = searchParams.get('order');
+
+        console.log(statusAppoint);
+        if(statusAppoint && statusAppoint !== "") {
+            const { rows }  = await pool.query(
+                `SELECT u.email, ap.status_appoint, ap.id_appointments, ap.created_at, ap.date_time 
+                FROM APPOINTMENTS ap 
+                JOIN USERS u ON u.ID_USER = ap.ID_USER 
+                WHERE ap.status_appoint = $1 
+                ORDER BY ap.created_at ${orderConsult === 'asc' ? 'ASC' : 'DESC'}`,
+                [statusAppoint]
+            );
+
+            if(rows.length === 0) {
+            return NextResponse.json([]);
+            }
+            return NextResponse.json(rows, {status: 200});
+        }
+
         const { rows }  = await pool.query
-        ('SELECT u.email, ap.id_appointments, ap.status_appoint, ap.id_appointments, ap.created_at, ap.date_time FROM APPOINTMENTS ap JOIN USERS u ON u.ID_USER = ap.ID_USER ');
+        ('SELECT u.email, ap.status_appoint, ap.id_appointments, ap.created_at, ap.date_time FROM APPOINTMENTS ap JOIN USERS u ON u.ID_USER = ap.ID_USER ');
         
         if(rows.length === 0) {
             return NextResponse.json( {message: "Nenhum agendamento feito"});
